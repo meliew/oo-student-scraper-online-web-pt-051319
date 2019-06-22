f4f113a0b1669e2de17a5d2f7cfa1cdab01537d5
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-22 21:30:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,11 @@ class Student
 
   def initialize(student_hash)
 
-    @@all << self
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    
 
+    
   end
 
   def self.create_from_collection(students_array)
