65ff316f94841d1c93be8e56b373739548622b35
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-22 21:46:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,14 @@ class Student
   def add_student_attributes(attributes_hash)
     # attributes_hash[:twitter] = [:twitter]
     @bio = attributes_hash[:bio]
+    @twitter = attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin]
+    @github = attributes_hash[:github]
+    @blog = attributes_hash[:blog]
+
+
+
+
 
 
   end
