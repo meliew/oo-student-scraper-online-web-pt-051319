fa6af21ac6c0e830d6be497d058264eddbd06dc1
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-22 21:47:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Scraper
 
   attr_accessor :students
-
+  
   def self.scrape_index_page(index_url)
 
     html = open(index_url)
@@ -16,9 +16,9 @@ class Scraper
 
     student_list.collect do |item|
       students << {
-      :name => item.css("h4.student-name").text,
-      :location => item.css("p.student-location").text,
-      :profile_url => item.css("a").attribute("href").value
+        :name => item.css("h4.student-name").text,
+        :location => item.css("p.student-location").text,
+        :profile_url => item.css("a").attribute("href").value
       }
 
     end
@@ -33,25 +33,25 @@ class Scraper
     student_bio = doc.css(".bio-content .description-holder").text.strip
     student_quote = doc.css(".profile-quote").text
     profile_info = {:bio => student_bio,
-    :profile_quote => student_quote}
+      :profile_quote => student_quote}
 
-    student_profile.collect do |profile|
-    profile.attribute("href").value
+      student_profile.collect do |profile|
+        profile.attribute("href").value
 
-    if profile.attribute("href").value.include? "twitter"
-      profile_info[:twitter] = profile.attribute("href").value
+        if profile.attribute("href").value.include? "twitter"
+          profile_info[:twitter] = profile.attribute("href").value
 
-    elsif profile.attribute("href").value.include? "github"
-      profile_info[:github] = profile.attribute("href").value
+        elsif profile.attribute("href").value.include? "github"
+          profile_info[:github] = profile.attribute("href").value
 
-    elsif profile.attribute("href").value.include? "linkedin"
-      profile_info[:linkedin] = profile.attribute("href").value
+        elsif profile.attribute("href").value.include? "linkedin"
+          profile_info[:linkedin] = profile.attribute("href").value
 
-    else profile_info[:blog] = profile.attribute("href").value
+        else profile_info[:blog] = profile.attribute("href").value
 
+        end
+      end
+      profile_info
     end
-  end
-  profile_info
-end
 
-end
+  end
